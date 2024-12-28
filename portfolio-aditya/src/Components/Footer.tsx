203a2621516a7a568ac76440eb1468035b82192e
pejeaditya <pejeaditya@gmail.com>
2024-12-29 00:49:16 +0530

    Adding files to project

---
+++ portfolio-aditya/src/Components/Footer.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import { Box, Container, Typography, IconButton, Divider } from '@mui/material';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import InstagramIcon from '@mui/icons-material/Instagram';
+
+const Footer = () => {
+  return (
+    <Box
+      component="footer"
+      sx={{
+        backgroundColor: 'black',
+        color: 'white',
+        padding: '20px 0',
+      }}
+    >
+      <Container maxWidth="xl" sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+        <Box>
+          <Typography variant="h6">Aditya Peje</Typography>
+          <Typography gutterBottom sx={{ color: "white", fontSize: 14 }}>Software Development Engineer</Typography>
+        </Box>
+        <Box>
+          <IconButton
+            component="a"
+            href="https://github.com/your-github-profile"
+            target="_blank"
+            rel="noopener noreferrer"
+            sx={{ color: 'white' }}
+          >
+            <GitHubIcon />
+          </IconButton>
+          <IconButton
+            component="a"
+            href="https://www.linkedin.com/in/your-linkedin-profile"
+            target="_blank"
+            rel="noopener noreferrer"
+            sx={{ color: 'white' }}
+          >
+            <LinkedInIcon />
+          </IconButton>
+          <IconButton
+            component="a"
+            href="https://www.instagram.com/your-instagram-profile"
+            target="_blank"
+            rel="noopener noreferrer"
+            sx={{ color: 'white' }}
+          >
+            <InstagramIcon />
+          </IconButton>
+        </Box>
+      </Container>
+      
+      <Container maxWidth="xl" sx={{ textAlign: 'center' }}>
+      <Divider sx={{ backgroundColor: 'white', margin: '30px 0' }} />
+        <Typography variant="body2">
+          © 2024 Aditya Peje. All rights reserved.
+        </Typography>
+      </Container>
+    </Box>
+  );
+};
+
+export default Footer;
